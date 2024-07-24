61d41a8973de41cfae0e995319c7c5ede8495399
DEOKJAE KWON <juintin@kakao.com>
2024-07-24 20:54:03 +0900

    feat: add a framework of the project

---
+++ src/main/java/com/example/vpapi/VpApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.vpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpApiApplication.class, args);
+	}
+
+}
