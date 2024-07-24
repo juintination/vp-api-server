41e59215bc01667f7a908e88db174f912b3caf70
DEOKJAE KWON <juintin@kakao.com>
2024-07-24 21:02:03 +0900

    feat: add LocalDateFormatter and CustomServletConfig

---
+++ src/main/java/com/example/vpapi/config/CustomServletConfig.java
@@ -0,0 +1,27 @@
+package com.example.vpapi.config;
+
+
+import com.example.vpapi.controller.formatter.LocalDateFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CustomServletConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new LocalDateFormatter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .maxAge(500)
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
+    }
+
+}
