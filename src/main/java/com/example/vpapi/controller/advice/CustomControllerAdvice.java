4eb0fbdf9ded951771a44c85f98a87338581415b
DEOKJAE KWON <juintin@kakao.com>
2024-09-10 00:58:56 +0900

    feat: add a additional exception method to CustomControllerAdvice
    Configured to send JSON string when CustomJWT and MemberService exceptions occur

--- src/main/java/com/example/vpapi/controller/advice/CustomControllerAdvice.java
+++ src/main/java/com/example/vpapi/controller/advice/CustomControllerAdvice.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import com.example.vpapi.util.CustomJWTException;
+import com.example.vpapi.util.MemberServiceException;
 
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -20,6 +22,17 @@ public class CustomControllerAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<?> handleIllegalArgumentException(MethodArgumentNotValidException e) {
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(Map.of("msg", e.getMessage()));
+    }
+
+    @ExceptionHandler(CustomJWTException.class)
+    protected ResponseEntity<?> handleJWTException(CustomJWTException e) {
+        String msg = e.getMessage();
+        return ResponseEntity.ok().body(Map.of("error", msg));
+    }
+
+    @ExceptionHandler(MemberServiceException.class)
+    protected ResponseEntity<?> handleMemberServiceException(MemberServiceException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
     }
 
 }
