4bc1168aad7bf2073c00ff18bdf3832ecdacb1b3
DEOKJAE KWON <juintin@kakao.com>
2024-09-21 21:19:48 +0900

    refactor: add Board, Reply, Heart associations to Member entity

--- src/main/java/com/example/vpapi/domain/Member.java
+++ src/main/java/com/example/vpapi/domain/Member.java
@@ -3,6 +3,8 @@ package com.example.vpapi.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Entity
 @Builder
 @AllArgsConstructor
@@ -25,6 +27,15 @@ public class Member extends BaseEntity {
 
     @Builder.Default
     private MemberRole memberRole = MemberRole.USER;
+
+    @OneToMany(mappedBy = "writer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Board> boards;
+
+    @OneToMany(mappedBy = "replier", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reply> replies;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Heart> hearts;
 
     public void changeRole(MemberRole memberRole) {
         this.memberRole = memberRole;
