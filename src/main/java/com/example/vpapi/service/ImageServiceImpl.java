e6bcdf99d9b7a40847c250eb8f0db5cc92abd791
DEOKJAE KWON <juintin@kakao.com>
2024-09-29 23:16:36 +0900

    Revert "style: remove unnecessary parts"
    This reverts commit 8bd97a75dd5f7db6822f565d686098ca6f9ca748.

--- src/main/java/com/example/vpapi/service/ImageServiceImpl.java
+++ src/main/java/com/example/vpapi/service/ImageServiceImpl.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @Log4j2
 @RequiredArgsConstructor
